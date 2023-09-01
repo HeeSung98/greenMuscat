eb5f5152b54bbaf1e2683a6f5acf7ea988476c6a
hyeiis <hyeis0807@gmail.com>
2023-09-01 11:39:15 +0900

    Add files

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const db = require('./models')
+const app = express()
+const PORT = 8000
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//router 분리
+const router = require('./router/GreenRouter')
+app.use('/', router)
+
+//오류처리
+app.use('*', (req, res) => {
+  res.status(404).render('404')
+})
+
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+  })
+})
---
+++ router/GreenRouter.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const controller = require('../controller/GreenController')
+
+// 메인 페이지
+router.get('/', controller.main)
+// 회원가입 (SignUp)
+router.get('/signUp', controller.signUp)
+router.post('/signUp', controller.postSignUp)
+// 로그인 (SignIn)
+rotuer.get('/signIn', controller.signIn)
+router.post('/signIn', controller.postSignIn)
+// 업로드 페이지 (Post)
+router.get('/aPost', controller.aPost)
+router.post('/aPost', controller.aPost)
+// 마이 페이지 (Profile)
+router.get('/profile:id', controller.profile)
+// 회원 정보 수정
+router.patch('/profile/edit', controller.editProfile)
+// 방 선택 페이지 (Select)
+router.get('/select', controller.select)
+// 방 메인 페이지 (Room)
+router.get('/room:id', controller.room)
+// 회원 탈퇴
+router.delete('/destroy', controller.destroy)
+
+module.exports = router
