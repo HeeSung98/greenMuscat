f019ef3caa2e3dc2275033c9115c04c26553172d
hyeiis <hyeis0807@gmail.com>
2023-09-05 10:28:49 +0900

    :sparkles: Feat: edit profile, delete profile, signout, profile
    edit feature about user

--- controller/GreenController.js
+++ controller/GreenController.js
@@ -4,13 +4,6 @@ const jwt = require('jsonwebtoken')
 const { mMember, mProfileImage, mRoom } = require('../models')
 const SECRET = 'mySecretKey'
 
-// 쿠키 설정
-// const cookieConfig = {
-//   httpOnly: true,
-//   maxAge: 60 * 1000, //1분
-// }
-// const SECRET = 'mySecret'
-
 //* GET
 // 로그인 전 메인 페이지 (홈 화면)
 const main = (req, res) => {
@@ -24,27 +17,40 @@ const signUp = (req, res) => {
 const signIn = (req, res) => {
   res.render('signin')
 }
-//TODO 로그아웃
-// const signOut = (req, res) => {}
+
 //TODO 게시글 페이지
 const board = (req, res) => {
   res.render('board')
 }
-//TODO 내 프로필
-// 토큰을 찾고 그 토큰에 해당하는 유저의 마이페이지
-// sign으로
+// 내 프로필
+// → 토큰을 찾고 그 토큰에 해당하는 유저의 마이페이지
 const profile = async (req, res) => {
-  const decodedToken = jwt.verify(token, SECRET)
   try {
-    await mMember
-      .findOne({
-        where: { email: decodedToken.email },
-      })
-      .then((result) => {
-        res.render('profile', { data: result })
-      })
+    const authHeader = req.headers.authorization
+    const [bearer, token] = authHeader.split(' ')
+    if (!authHeader) {
+      res.json({ result: false, message: '인증 정보가 필요합니다.' })
+      return
+    }
+    const decodedToken = jwt.verify(token, SECRET)
+    if (bearer !== 'Bearer') {
+      res.json({ result: false, message: '인증 방식이 틀렸습니다.' })
+      return
+    }
+    const memberProfile = await mMember.findOne({
+      where: { email: decodedToken.email },
+    })
+    if (memberProfile !== null) {
+      // res.render('profile', { data: memberProfile })
+      res.json({ result: memberProfile })
+      console.log('회원 정보 조회 성공', memberProfile)
+    }
   } catch (error) {
     console.log(error)
+    return res.json({
+      result: false,
+      message: '회원 정보 조회 중 오류가 발생했습니다.',
+    })
   }
 }
 
@@ -60,7 +66,6 @@ const select = (req, res) => {
 }
 //TODO 선택한 방의 메인 페이지
 const room = (req, res) => {
-  // findOne ?
   res.render('room')
 }
 //TODO 선택한 방의 게시물 페이지 (board)
@@ -97,27 +102,49 @@ const postSignIn = async (req, res) => {
   try {
     const { email, password } = req.body
     // 사용자 조회
-    const result = await mMember.findOne({
+    const user = await mMember.findOne({
       where: { email },
     })
-    if (!result) {
+    if (!user) {
       res.json({ result: false, message: '사용자가 존재하지 않습니다' })
     }
     // 비밀번호 확인
-    const compare = await comparePassword(password, result.password)
+    const compare = await comparePassword(password, user.password)
     console.log(compare)
     if (compare) {
       // 비밀번호 일치
       // res.cookie('isLoggin', true, cookieConfig);
       // req.session.userInfo = { name: user.name, id: user.id };
-      const token = jwt.sign({ email }, SECRET) // email에 해당하는 token 발급
-      res.json({ result: true, token, data: compare })
+      const token = jwt.sign({ email }, SECRET, { expiresIn: '3d' }) // email에 해당하는 token 발급. 3일 유효
+      // 해당 유저에게 token 할당 후 저장
+      user.token = token
+      user.save((error, user) => {
+        if (error) return res.status(400).json({ error: 'something wrong' })
+      })
+      // db에 토큰 저장한 후에는 cookie에 토큰 저장하여 이용자 식별
+      return res
+        .cookie('x_auth', user.token, {
+          // 3일간 유지
+          maxAge: 1000 * 60 * 60 * 24 * 3,
+          httpOnly: true,
+        })
+        .status(200)
+        .json({ loginSuccess: true, token })
+      // res.json({ result: true, token, data: compare })
     } else {
-      res.json({ result: false, message: '비밀번호가 일치하지 않습니다' })
+      return res
+        .status(403)
+        .json({ loginSuccess: false, message: '비밀번호가 일치하지 않습니다' })
     }
   } catch (err) {
     console.log(err)
   }
+}
+
+// 로그아웃
+// 로그아웃 버튼 누르면 생성되었던 토큰 사라지게 ""
+const postSignOut = (req, res) => {
+  res.cookie('x_auth', '').json({ logoutSuccess: true })
 }
 
 // 방 생성하는 페이지
@@ -180,10 +207,20 @@ const editProfile = async (req, res) => {
 
 //* DELETE
 // 회원 탈퇴
-const deleteProfile = (req, res) => {
-  const { email } = req.body
-  mMember.destroy({ where: { email } }).then(() => {
-    // res.clearCookie('testCookie')
+const deleteProfile = async (req, res) => {
+  const authHeader = req.headers.authorization
+  const [bearer, token] = authHeader.split(' ')
+  if (!authHeader) {
+    res.json({ result: false, message: '인증 정보가 필요합니다.' })
+    return
+  }
+  const decodedToken = jwt.verify(token, SECRET)
+  if (bearer !== 'Bearer') {
+    res.json({ result: false, message: '인증 방식이 틀렸습니다.' })
+    return
+  }
+  await mMember.destroy({ where: { email: decodedToken.email } }).then(() => {
+    res.clearCookie('x_auth')
     // req.session.destroy()
     res.json({ result: true, message: '회원 탈퇴 성공' })
   })
@@ -193,6 +230,7 @@ module.exports = {
   main,
   signUp,
   signIn,
+  postSignOut,
   profile,
   board,
   select,
