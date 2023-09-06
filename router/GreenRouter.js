289500c6e9dc58bf34ccd7fa3cc84d2b8a523038
HeeSung98 <wlfkrdl2@gmail.com>
2023-09-06 09:26:59 +0900

    model 수정

--- router/GreenRouter.js
+++ router/GreenRouter.js
@@ -3,53 +3,41 @@ const router = express.Router()
 const controller = require('../controller/GreenController')
 
 // 메인 페이지
-router.get('/main', controller.main)
+router.get('/', controller.main)
 
 // 회원가입 (SignUp)
-router.get('/main/signup', controller.signUp)
-router.post('/main/signup', controller.postSignUp)
+router.get('/signup', controller.signUp)
+router.post('/signup', controller.postSignUp)
 
 // 로그인 (SignIn)
-router.get('/main/signin', controller.signIn)
-router.post('/main/signin', controller.postSignIn)
-
-// 마이 페이지 (Profile)
-router.get('/main/profile', controller.profile)
-
-// 회원 정보 수정
-router.patch('/main/profile/edit', controller.editProfile)
-
-// 회원 탈퇴
-router.delete('/main/profile/destroy', controller.deleteProfile)
-
-// 로그아웃 (SignOut)
-router.post('/signout', controller.postSignOut)
+router.get('/signin', controller.signIn)
+router.post('/signin', controller.postSignIn)
 
 // 방 생성 페이지(roomadd)
-router.get('/main/add', controller.roomAdd)
-router.post('/main/add', controller.postRoomAdd)
+router.get('/roomadd', controller.roomAdd)
+router.post('/roomadd', controller.postRoomAdd)
 
-// 방 입장 (RoomEntrance)
-router.post('/main/entrance')
+// 업로드 페이지 (Post)
+router.get('/board:rno', controller.board)
+router.post('/board:rno', controller.postBoard)
+
+// 마이 페이지 (Profile)
+router.get('/profile:id', controller.profile)
+
+// 회원 정보 수정
+router.patch('/profile/edit', controller.editProfile)
+
+// 방 선택 페이지 (Select)
+router.get('/select', controller.select)
 
 // 방 메인 페이지 (Room)
 router.get('/room:id', controller.room)
 
-// 방 게시물 목록 페이지 (Board)
-router.get('/room/board', controller.room)
-
-// 게시물 업로드 페이지 (Post)
-router.get('/room/board/register', controller.board)
-router.post('/room/board/register', controller.postBoard)
-
-// 게시물 수정 페이지 (Post)
-router.get('/room/board/register', controller.board)
-router.post('/room/board/register', controller.postBoard)
-
-// 게시물 삭제 페이지 (Post)
-router.post('/room/board/register', controller.postBoard)
+// 회원 탈퇴
+router.delete('/destroy', controller.deleteProfile)
 
 // 관리자 페이지
+router.get('/admin', controller.admin)
 router.post('/admin', controller.postAdmin)
 
 module.exports = router
