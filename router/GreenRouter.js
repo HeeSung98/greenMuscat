305518cb2bd651315164f23dd7d4df56fc21289f
hyeiis <hyeis0807@gmail.com>
2023-09-08 12:27:26 +0900

    edit controller

--- router/GreenRouter.js
+++ router/GreenRouter.js
@@ -122,7 +122,7 @@ router.delete('/room/board/remove', deleteController.removeBoard)
 // 댓글 페이지
 // router.get('/room/board/reply', controller.reply)
 // 댓글 정보 보내기
-// router.post('/room/board/reply', controller.postReply)
+outer.post('/room/board/reply', postController.postReply)
 // 댓글 달기
 router.post('/room/board/reply/register', postController.postRegisterReply)
 // 댓글 수정
