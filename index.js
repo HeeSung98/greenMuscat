ab3731ae76a13c8709aca1a177ffabc795ccea70
sonys95 <sonys95@naver.com>
2023-09-12 09:46:15 +0900

    commit

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.set('views', './views')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(express.static('./style'))
+app.use(express.static('./img'))
 
 //router 분리
 const router = require('./router/GreenRouter')
