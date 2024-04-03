90145ce4755093d7eed03a10844f32f3adc6655c
HeeSung98 <wlfkrdl2@gmail.com>
2024-04-03 16:10:56 +0900

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const db = require('./models')
 const app = express()
 const path = require('path')
-const PORT = 8000
+const PORT = 8080
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
